7bddb3ad920f026e53dd5cf8f35bcf1580c936b0
Marco M <mam785@txstate.edu>
2025-07-14 02:31:15 -0500

    Finished adding code to Bishop class, with functional moves

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -1,31 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Bishop class
 public class Bishop extends Piece{ 
     private String color;
     private Position position;
+    
+    // constructor
     public Bishop(String color, Position position) {
         this.color = color;
         this.position = position;
     }
+
+    // getter and setter
     @Override
     public String getColor() {
         return color;
     }
-
+    
     @Override
     public Position getPosition() {
         return position;
     }
-
+    // set position 
     @Override
     public void setPosition(Position position) {
         this.position = position;
     }
-
+    // possible moves
     @Override
     public List<Position> possibleMoves(Board board) {
         // Implement logic for bishop's possible moves
-        return List.of(); // Placeholder for actual implementation
-    }
+        List<Position> moves = new ArrayList<>();
 
+        // Checks all 4 diagonal directions
+        int[] rowDirs = {1,1,-1,-1}; 
+        int[] colDirs = {1,-1,1,-1}; 
+
+
+        // current position 
+        int row = position.getRow();
+        int col = position.getCol(); 
+        
+        for (int i = 0; i < 4; i++){
+            int newRow = row + rowDirs[i];
+            int newCol = col + colDirs[i];
+
+            // implement isValidPosition in board class
+            //public boolean isValidPosition(int row, int col) {
+            //return row >= 0 && row < 8 && col >= 0 && col < 8;
+            //}
+
+            // while loop to check all possible moves in the direction
+            while (board.isValidPosition(newRow, newCol)) {
+                Position newPos = new Position(newRow, newCol);
+                Piece pieceAtPosition = board.getPiece(newPos);
+
+                if (pieceAtPosition == null) {
+                    moves.add(newPos);
+                } else {
+                    if (!pieceAtPosition.getColor().equals(this.color)) {
+                        moves.add(newPos);
+                    }
+                    break;
+
+                }
+                // move in the direction 
+                newRow += rowDirs[i];
+                newCol += colDirs[i];
+            }
+
+        }
+        // return all possible moves
+        return moves;
+    }
+    // toString method
     @Override
     public String toString() {
         return "Bishop{" + "color='" + color + '\'' + ", position=" + position + '}';
@@ -33,3 +82,4 @@ public class Bishop extends Piece{
 
     
 }
+
