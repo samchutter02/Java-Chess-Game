8e53305ea1d291c012081628a9d6373431766e23
Marco M <mam785@txstate.edu>
2025-07-15 02:14:58 -0500

    Added Knight moves with comments

--- pieces/Knight.java
+++ pieces/Knight.java
@@ -1,17 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
+// Knight class
 public class Knight extends Piece{
     private String color;
     private Position position;
 
+    // Constructor
     public Knight(String color, Position position) {
         this.color = color;
         this.position = position;
     }
 
+    // getter and setter
     @Override
     public String getColor() {
         return color;
     }
 
+    
     @Override
     public Position getPosition() {
         return position;
@@ -22,12 +29,45 @@ public class Knight extends Piece{
         this.position = position;
     }
 
+
+    // possible moves
     @Override
     public List<Position> possibleMoves(Board board) {
-        // Implement logic for knight's possible moves
-        return List.of(); // Placeholder for actual implementation
+   
+        // Possible moves
+        List<Position> moves = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getCol();
+
+
+        // checks all 8 directions
+        int[] rowDirs = {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] colDirs = {1, 2, 2, 1, -1, -2, -2, -1};
+
+        // while loop to check all possible moves in the direction
+        for (int i = 0; i < 8; i++) {
+            int newRow = row + rowDirs[i];
+            int newCol = col + colDirs[i];
+
+            // checks if the new position is valid
+            if (board.isValidPosition(newRow, newCol)) {
+                Position newPos = new Position(newRow, newCol);
+                Piece pieceAtPosition = board.getPiece(newPos);
+
+                // checks if there is a piece at the new position
+                if (pieceAtPosition == null || !pieceAtPosition.getColor().equals(getColor())) {
+                    moves.add(newPos);
+                }
+            }
+        }
+
+
+
+        return moves;
     }
 
+
+    // toString/print
     @Override
     public String toString() {
         return "Knight{" + "color='" + color + '\'' + ", position=" + position + '}';
