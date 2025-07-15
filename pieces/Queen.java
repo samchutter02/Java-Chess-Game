74bc03a235d83d51a657b6d4fba059fdf2568e64
Marco M <mam785@txstate.edu>
2025-07-15 02:12:21 -0500

    Added queen possible moves with comments

--- pieces/Queen.java
+++ pieces/Queen.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// Queen class
 public class Queen extends Piece {
     private String color;
     private Position position;
@@ -8,7 +9,7 @@ public class Queen extends Piece {
         this.color = color;
         this.position = position;
     }
-
+// getter and setter
     @Override
     public String getColor() {
         return color;
@@ -22,12 +23,48 @@ public class Queen extends Piece {
     public void setPosition(Position position) {
         this.position = position;
     }
+    // possible moves
     @Override
     public List<Position> possibleMoves(Board board) {
-        // Implement logic for queen's possible moves
         List<Position> moves = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getCol();
+
+    // checks all 8 directions
+        int[] rowDirs = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] colDirs = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        // while loop to check all possible moves in the direction
+        for (int i = 0; i < 8; i++) {
+            int newRow = row + rowDirs[i];
+            int newCol = col + colDirs[i];
+
+            // check if the new position is valid
+            while (board.isValidPosition(newRow, newCol)) {
+                Position newPos = new Position(newRow, newCol);
+                Piece pieceAtPosition = board.getPiece(newPos);
+
+                // if the new position is empty, add it to the list
+                if (pieceAtPosition == null ) {
+                    moves.add(newPos);
+                } else{ // if the new position is occupied by a piece of the opposite color, add it to the list
+                    if(!pieceAtPosition.getColor().equals(this.color)){
+                        moves.add(newPos);
+                    }
+                    break;
+                }
+                // update new row and col
+                newRow += rowDirs[i];
+                newCol += colDirs[i];
+            }
+        }
+        
+        
+
+        // return possible moves
         return moves;
     }
+    // toString/print
     @Override
     public String toString() {
         return "Queen{" + "color='" + color + '\'' + ", position=" + position + '}';
