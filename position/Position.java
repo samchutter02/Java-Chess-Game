39848bd98e3d0b493d6fc92357885e2a848030b9
Marco M <mam785@txstate.edu>
2025-07-16 17:22:32 -0500

    Added missing code on the position.java file

--- position/Position.java
+++ position/Position.java
@@ -1,5 +1,45 @@
+package position;
+
 public class Position{
-    int[][] position;
+    private int row;
+    private int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+
+    @Override
+    public boolean equals(Object start) {
+        if (this == start) {
+            return true;
+        }
+        if (!(start instanceof Position)) {
+            return false;
+        }
+        Position other = (Position) start;
+        return row == other.row && col == other.col;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + row + ", " + col + ")";
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + col;
+        result = prime * result + row;
+        return result;
+    }
     
 
 }
