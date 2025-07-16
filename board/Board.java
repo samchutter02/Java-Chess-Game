137766c6576c2083865ef5f69da66f1b764c8ba0
Marco M <mam785@txstate.edu>
2025-07-16 02:19:08 -0500

    added check/checkmate option as well as fixed some syntax errors

--- board/Board.java
+++ board/Board.java
@@ -1,5 +1,9 @@
 import pieces.Knight;
 import pieces.Pawn;
+import pieces.Queen;
+import pieces.Rook; 
+import pieces.Bishop;
+import pieces.King;
 import pieces.Piece;
 import position.Position;
 
@@ -21,7 +25,7 @@ public class Board {
         board[0][6] = new Knight("black", new Position(0,6));
         board[0][7] = new Rook("black", new Position(0,7)); // top right position
 
-        for(int i=0; i<8, i++){
+        for(int i=0; i<8; i++){
             board[1][i] = new Pawn("black", new Position(1,i));
         }
 
@@ -34,7 +38,7 @@ public class Board {
         board[7][6] = new Knight("white", new Position(7,6));
         board[7][7] = new Rook("white", new Position(7,7)); // bottom right position
 
-        for(int i=0; i<8, i++){
+        for(int i=0; i<8; i++){
             board[6][i] = new Pawn("white", new Position(6,i));
         }
     }
@@ -43,6 +47,10 @@ public class Board {
         board = new Piece[8][8]; //array not including labels on TL or T
         initializeBoard();
     }
+    public boolean isValidPosition(int row, int col) {
+    return row >= 0 && row < 8 && col >= 0 && col < 8;
+}
+
 
     public Piece getPiece(Position pos){
         // FIXME
@@ -62,6 +70,12 @@ public class Board {
             System.out.println("No piece at that position.");
             return false;
         }
+        Piece targetPiece = getPiece(to);
+        if (targetPiece != null && targetPiece.getColor().equals(movingPiece.getColor())) {
+            System.out.println("You can't capture your own piece.");
+            return false;
+        }
+
         board[to.getRow()][to.getCol()] = movingPiece;
         board[from.getRow()][from.getCol()] = null;
         movingPiece.setPosition(to);
@@ -75,4 +89,30 @@ public class Board {
             System.out.print((8-row)+ " ");
             for(int col=0; col<8; col++) {
                 Piece p = board[row][col];
-             
+                if(p != null){
+                    System.out.print(p + " ");
+                }else{
+                    if((row+col)%2==0){ // basically if 0 then black square, else white square, even/odd grid
+                        System.out.print("⬛"); 
+                    } else {
+                        System.out.print("⬜"); 
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public Position findKingPosition(String color) {
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            Piece piece = board[row][col];
+            if (piece instanceof King && piece.getColor().equals(color)) {
+                return new Position(row, col);
+            }
+        }
+    }
+    return null; // Should never happen in a valid game
+}  
+
+}
